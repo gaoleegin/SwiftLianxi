b6eb10e0629423dc5f5f66bb184588b16d95bd8f
ershixiong <lijungao1999@163.com>
2015-10-19 15:10:13 +0800

    首页的整体的布局，中间加号按钮的点击

---
+++ SwiftLianxi/SwiftLianxi/Classes/Mudule/Main/MainTabBar.swift
@@ -0,0 +1,21 @@
+//
+//  MainTabBar.swift
+//  SwiftLianxi
+//
+//  Created by 高李军 on 15/10/19.
+//  Copyright © 2015年 LJLianXi. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBar: UITabBar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
