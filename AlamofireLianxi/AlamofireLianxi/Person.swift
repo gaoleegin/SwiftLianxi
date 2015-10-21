52648a49f30fb8d195a1cad6970630531730d5ed
ershixiong <lijungao1999@163.com>
2015-10-21 08:53:53 +0800

    添加Person类

---
+++ AlamofireLianxi/AlamofireLianxi/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  AlamofireLianxi
+//
+//  Created by 高李军 on 15/10/20.
+//  Copyright © 2015年 LJLianXi. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name:String?
+    var age:Int?
+    
+    
+}
--- AlamofireLianxi/AlamofireLianxi/ViewController.swift
+++ AlamofireLianxi/AlamofireLianxi/ViewController.swift
@@ -14,11 +14,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         Alamofire.request(.GET, "http://httpbin.org/get")
             .responseString { response in
                 print("Success: \(response.result.isSuccess)")
                 print("Response String: \(response.result.value)")
         }
+        
+        Alamofire.request(.GET, "").responseString { response in
+            response.result.isSuccess
+            response.result.value
+            response.result
+        }
+        
+        Alamofire.request(.GET, "").responseString { respons in
+            
+        }
+        
+        
         
         
         Alamofire.request(.GET, "http://httpbin.org/get", parameters: ["foo": "bar"])
