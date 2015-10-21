c11ab275991c0f2ca7d6251a8cdf003fce4ce586
ershixiong <lijungao1999@163.com>
2015-10-21 17:07:42 +0800

    第三方授权页面的分析

--- SwiftLianxi/SwiftLianxi/Classes/Mudule/OAuth/OAuthViewController.swift
+++ SwiftLianxi/SwiftLianxi/Classes/Mudule/OAuth/OAuthViewController.swift
@@ -16,30 +16,83 @@ class OAuthViewController: UIViewController {
     let WB_Redirect_URI     = "http://itheima.com"
     
     
-    var webView: UIWebView {
-        return view as! UIWebView
-    }
+    @IBOutlet weak var webView: UIWebView!
+   
    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadOAuthPage()
+        webView.delegate = self;
         // Do any additional setup after loading the view.
     }
     
     private func loadOAuthPage(){
+        
+        SVProgressHUD.show()
+        
         let urlString = "https://api.weibo.com/oauth2/authorize?client_id=\(WB_Client_ID)&redirect_uri=\(WB_Redirect_URI)"
         webView.loadRequest(NSURLRequest(URL: NSURL(string: urlString)!))
-        
-        
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
+
+extension OAuthViewController : UIWebViewDelegate{
+    func webViewDidFinishLoad(webView: UIWebView) {
+        SVProgressHUD.dismiss()
+    }
     
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        //第一次进入的时间的urlString
+        //https://api.weibo.com/oauth2/authorize?client_id=479651210&redirect_uri=http://itheima.com
+        
+        //输入用户名和密码以后，点击确定以后,点击OAuth授权以后的urlString
+        //https://api.weibo.com/oauth2/authorize
+        
+        //https://api.weibo.com/oauth2/authorize#
+        
+        //https://api.weibo.com/oauth2/authorize
+        
+        //点击允许以后所做的事情
+        //http://itheima.com/?code=3c73b66a3a7dc122a52ebcae32525e29
+        
+        //https://api.weibo.com/oauth2/authorize#
+        
+        //https://api.weibo.com/oauth2/authorize
+        
+        let urlString = request.URL!.absoluteString
+        
+        //let query = request.URL!.query
+        
+        //如果是官方的页面继续加载
+        if urlString.hasPrefix("https://api.weibo.com") {
+            return true
+        }
+        
+        if !urlString.hasPrefix(WB_Redirect_URI){
+            return false
+        }
+        
+        //如果执行到这里，一定是
+        
+        let codeStr = "code="
+        let query = request.URL!.query!
+        
+        if query.hasPrefix(codeStr) {
+            
+            let code = (query as NSString).substringFromIndex(codeStr.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+            
+            print("验证码＝" + code)
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+        return true
 
-   
-
+    }
+    
 }
